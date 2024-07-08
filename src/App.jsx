b54a0b8e7262b5546855ed27f42d0fb6a76a737e
Benson Manzano <bfmanzan@uci.edu>
2024-07-07 23:53:00 -0700

    Created a hashmap that correlates that day to the specific events that are occurring that day making it easier to iterate through and set dates and times for each event in the ics file

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,8 @@ export default function App() {
   const [data, setData] = useState(null);
   let [baseDay, setBaseDay] = useState(null);
   const [loading, setLoading] = useState(false);
+
+  // values for the start of the quarter 
   const dates = [new Date(2023, 8, 28), new Date(2024, 0, 8), new Date(2024, 3, 1)];
 
   const handleUpload = (e) => {
@@ -41,7 +43,7 @@ export default function App() {
       console.log(response.data['ParsedResults'][0]['TextOverlay']['Lines']);
       setData(response.data['ParsedResults'][0]['TextOverlay']['Lines']);
       console.log('finished')
-      fetchDays();
+      fetchFile();
     } catch (error) {
       console.error(error);
       setLoading(false);
@@ -50,12 +52,12 @@ export default function App() {
     setLoading(false);
   };
 
-  const fetchDays = async () => {
+  const fetchFile = async () => {
     setLoading(true);
     try {
       // I would get the day that user is doing this on
       const now = new Date();
-
+      
       for(let i = 0; i < dates.length; i++){
         if(!(now > dates[i])) {
           setBaseDay(dates[i]);
@@ -68,22 +70,47 @@ export default function App() {
       }
 
       const weekNumber = parseInt(data[0]['LineText'].split(' ')[1]);
-      let startDate = null;
-      for(let i = 0; i < weekNumber; i++){
-        startDate = date.addDays(baseDay, i * 7);
+
+      let curDate = date.addDays(baseDay, weekNumber * 7);
+
+      for(let i = 0; i < 5; i++){
+        // add events function
+        curDate = date.addDays(curDate, 1);
       }
 
-      console.log(startDate);
-      // and if that value is greater compared to that of the dates provided as the quarter starters
-      // then you would go for the latest data to be started
-      // from tehre append from that start date to the given week number (do add (7 days) from the start date for x (the week nubmer) times)
-      // from there this would consist of the dates from monday to sunday for that whole week
-      // add those registed dates to an array and retrieve that date when you iterate through the line results [Monday: date, Tuesday: date, ...]
-      // add the events to the ics calendar object 
-      // make a feature to download the ics file 
+      console.log(curDate);
+      console.log(data[1]['Words']['length'])
+      formatDaysToEvents();
+      
     } catch {
       console.log('error');
     }
+  }
+
+  const formatDaysToEvents = () => {
+     // format the data to make it where each day has their corresponding 
+    // use i = 1 to skip the week number
+    let events = new Map();
+    let day = "";
+    let content = [];
+
+    for(let i = 1; i < data.length; ++i) {
+      if (data[i]['Words']['length'] === 1) {
+        if (day !== "") {
+          events.set(day, content);
+          day = "";
+          content = []
+        }
+
+        day = data[i]['LineText'];
+      }
+      else {
+        content.push(data[i]['LineText'])
+      }
+    }
+
+    console.log(events)
+     
   }
 
 
